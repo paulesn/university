30f0cc988f0f11120e166a4682a9b7432655597b
Fabian Grote <fabi238.g@gmail.com>
2018-04-07 22:40:57 +0200

    So_22.40

--- src/de/university/data/rooms/ComputerRoom.java
+++ src/de/university/data/rooms/ComputerRoom.java
@@ -6,7 +6,7 @@ public class ComputerRoom extends Room{
     //----constructor----
 	
 	public ComputerRoom(){
-		this.setName("Computer roomzz");
+		this.setName("Computer room");
 		this.setCosts(6000);
 		this.setSize(RoomSize.BIG);
 		this.setKnowledgeBonus(15);
