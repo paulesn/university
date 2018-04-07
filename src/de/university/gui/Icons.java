551e27486c17e350bb5afcce18cb8332ab532740
Fabian Grote <fabi238.g@gmail.com>
2018-04-08 01:40:24 +0200

    So_23.50

--- src/de/university/gui/Icons.java
+++ src/de/university/gui/Icons.java
@@ -3,13 +3,61 @@ package de.university.gui;
 import de.university.data.rooms.Room;
 
 import javax.swing.*;
+import java.lang.reflect.Array;
 
 public class Icons {
 
     public static ImageIcon get(int y, int x, Room room){
+        ImageHandler image = new ImageHandler();
 
-        String path = room.getPath()+"-"+x+"-"+y+".png";
-        ImageIcon icon = new ImageIcon(path);
-        return icon;
+        int sizex;
+        int sizey;
+
+        switch(room.getSize()) {
+            case MICRO:
+                ImageHandler image = new ImageHandler(1,1,room.getPath());
+                sizex=1;
+                sizey=1;
+                break;
+            case SMALL:
+                ImageHandler image = new ImageHandler(1,2,room.getPath());
+                sizex=1;
+                sizey=2;
+                break;
+            case NORMAL:
+                ImageHandler image = new ImageHandler(2,2,room.getPath());
+                sizex=2;
+                sizey=2;
+                break;
+            case MEDIUM:
+                ImageHandler image = new ImageHandler(2,3,room.getPath());
+                sizex=2;
+                sizey=3;
+                break;
+            case BIG:
+                ImageHandler image = new ImageHandler(3,3,room.getPath());
+                sizex=3;
+                sizey=3;
+                break;
+            case BIGGER:
+                ImageHandler image = new ImageHandler(4,4,room.getPath());
+                sizex=4;
+                sizey=4;
+                break;
+            case GIGANTIC:
+                ImageHandler image = new ImageHandler(5,5,room.getPath());
+                sizex=5;
+                sizey=5;
+                break;
+        }
+
+        for(int i=0; i<sizex; i++){
+            for(int j=0; j<sizey; j++){
+                image.getImagePiece(i,j);
+                String path=room.getPath()+"-"+i"-"+y+".png";
+                ImageIcon icon = new ImageIcon(path);
+                return icon;
+            }
+        }
     }
 }
