2be3081516325d3587757ebebc87712a25e5f865
Fabian Grote <fabi238.g@gmail.com>
2018-04-08 00:50:48 +0200

    So_23.50

--- src/de/university/gui/Icons.java
+++ src/de/university/gui/Icons.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 public class Icons {
 
     public static ImageIcon get(int y, int x, Room room){
+
         String path = room.getPath()+"-"+x+"-"+y+".png";
         ImageIcon icon = new ImageIcon(path);
         return icon;
