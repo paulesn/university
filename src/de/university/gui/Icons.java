dd88f3924ad1be0d9cd2b83c7b64ef0d8707b5ea
Fabian Grote <fabi238.g@gmail.com>
2018-04-08 02:52:38 +0200

    Mo_2.52

--- src/de/university/gui/Icons.java
+++ src/de/university/gui/Icons.java
@@ -17,37 +17,37 @@ public class Icons {
                 image = new ImageHandler(1,1,picture);
                 sizex=1;
                 sizey=1;
-                return image.getImagePiece(y,x);
+                return image.getImagePiece(x,y);
             case SMALL:
                 image = new ImageHandler(1,2,picture);
                 sizex=1;
                 sizey=2;
-                return image.getImagePiece(y,x);
+                return image.getImagePiece(x,y);
             case NORMAL:
                 image = new ImageHandler(2,2,picture);
                 sizex=2;
                 sizey=2;
-                return image.getImagePiece(y,x);
+                return image.getImagePiece(x,y);
             case MEDIUM:
                 image = new ImageHandler(2,3,picture);
                 sizex=2;
                 sizey=3;
-                return image.getImagePiece(y,x);
+                return image.getImagePiece(x,y);
             case BIG:
                 image = new ImageHandler(3,3,picture);
                 sizex=3;
                 sizey=3;
-                return image.getImagePiece(y,x);
+                return image.getImagePiece(x,y);
             case BIGGER:
                 image = new ImageHandler(4,4,picture);
                 sizex=4;
                 sizey=4;
-                return image.getImagePiece(y,x);
+                return image.getImagePiece(x,y);
             default:
                 image = new ImageHandler(5,5,picture);
                 sizex=5;
                 sizey=5;
-                return image.getImagePiece(y,x);
+                return image.getImagePiece(x,y);
         }
     }
 }
